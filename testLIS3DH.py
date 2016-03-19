9b004f8da33c982a8fb8419e1635a4d86efc1a51
MalSmalley <Mal.smalley@sky.com>
2016-03-19 09:34:32 +0000

    Added files via upload

---
+++ testLIS3DH.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+from LIS3DH import LIS3DH
+from time import sleep
+
+def clickcallback(channel):
+	# interrupt handler callback   
+	print "Interrupt detected"
+	click = sensor.getClick()
+	print "Click detected (0x%2X)" % (click)
+	if (click & 0x10): print " single click"
+	if (click & 0x20): print " double click"
+
+
+if __name__ == '__main__':
+	sensor = LIS3DH(debug=True)
+	sensor.setRange(LIS3DH.RANGE_2G)
+	sensor.setClick(LIS3DH.CLK_SINGLE,80,mycallback=clickcallback)
+
+	print "Starting stream"
+	while True:
+	   
+		x = sensor.getX()
+		y = sensor.getY()
+		z = sensor.getZ()
+
+# raw values
+		print "\rX: %.6f\tY: %.6f\tZ: %.6f" % (x,y,z)
+		sleep(0.1)
+		
+# click sensor if polling & not using interrupt		
+#		click = sensor.getClick()
+#		if (click & 0x30) :
+#			print "Click detected (0x%2X)" % (click)
+#			if (click & 0x10): print " single click"
+#			if (click & 0x20): print " double click"
